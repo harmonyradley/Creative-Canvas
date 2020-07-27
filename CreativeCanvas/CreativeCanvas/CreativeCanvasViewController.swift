60c32d60f59ed0c856796fa475824652aceee208
Harmony Radley <harmonyradley99@gmail.com>
2020-07-27 14:59:03 -0400

    Added methods to ViewController

--- CreativeCanvas/CreativeCanvas/CreativeCanvasViewController.swift
+++ CreativeCanvas/CreativeCanvas/CreativeCanvasViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class CreativeCanvasViewController: UIViewController {
 
+
+    @IBOutlet var DrawHereLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+
+
+    // MARK: - Bar Button Items Methods
+
+    @IBAction func SaveButtonTapped(_ sender: Any) {
+
+    }
+
+
+    @IBAction func AddPhotoButtonTapped(_ sender: Any) {
+
+    }
+
+    // MARK: - Size of Drawing Line Methods
+
+    @IBAction func smallSizeOfDrawingLine(_ sender: Any) {
+
+    }
+
+    @IBAction func mediumSizeOfDrawingLine(_ sender: Any) {
+
+    }
+
+    @IBAction func largeSizeOfDrawingLine(_ sender: Any) {
+
+    }
+
+    // MARK: - Features Methods
+
+    @IBAction func changeColorOfDrawingLine(_ sender: Any) {
+
+    }
+
+    @IBAction func StickersButton(_ sender: Any) {
+
+    }
+
+    @IBAction func addTextButton(_ sender: Any) {
+        
+    }
 
 }
 
