d3a83b783e8df6274d354781a1598a3edcfcda94
Harmony Radley <harmonyradley99@gmail.com>
2020-07-27 14:26:55 -0400

    Created Xcode Project

---
+++ CreativeCanvas/CreativeCanvas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreativeCanvas
+//
+//  Created by Harmony Radley on 7/27/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
